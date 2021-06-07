893d86cc3850aa2a3080855c9ff59fb47b883708
Brian Broeking <bbroeking456@gmail.com>
2021-06-07 17:23:35 -0500

    first part of fixing error

--- src/app/discover/discover.component.spec.ts
+++ src/app/discover/discover.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EthersService } from '../services/ethers.service';
 import { TileDataService } from '../services/tile-data.service';
+import { TileGeneratorService } from '../services/tile-generator.service';
 
 import { DiscoverComponent } from './discover.component';
 
@@ -12,7 +13,8 @@ describe('DiscoverComponent', () => {
     await TestBed.configureTestingModule({
       providers: [
         { provide: EthersService, useValue: {}},
-        { provide: TileDataService, useValue: {}}
+        { provide: TileDataService, useValue: {}},
+        { provide: TileGeneratorService, useValue: {}}
       ],
       declarations: [ DiscoverComponent ]
     })
