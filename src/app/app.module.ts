0555ad5970fcc404b7f436894588f8c8b3c059dc
Brian Broeking <bbroeking456@gmail.com>
2021-06-21 00:47:21 -0500

    add planning table + garrison / fix app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,13 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Provider } from './services/ethers-utils/web3-provider';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExplorerComponent } from './explorer/explorer.component';
 import { InitalizeService } from './services/initalize.service';
 import { EthersService } from './services/ethers.service';
 import { HttpClientModule } from '@angular/common/http';
-import { MapComponent } from './map/map.component';
-
-import { BrowserComponent } from './browser/browser.component';
 import { MatTableModule } from '@angular/material/table';
-import { RouterModule, Routes } from '@angular/router';
-import { ParcelDetailsComponent } from './parcel-details/parcel-details.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ParcelTileComponent } from './parcel-tile/parcel-tile.component';
-
+import { Routes } from '@angular/router';
 
 import {MatButtonModule} from '@angular/material/button'
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
@@ -25,29 +16,39 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatDialogModule} from '@angular/material/dialog';
 
-
-
 // Firebase
 import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { providers } from 'ethers';
-import { TileDataService } from './services/tile-data.service';
-import { HoverTileMenuComponent } from './hover-tile-menu/hover-tile-menu.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { StructureComponent } from './structure/structure.component';
-import { StructureDetailsComponent } from './structure-details/structure-details.component';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { LoginComponent } from './login/login.component';
 import { AngularFireFunctionsModule, USE_EMULATOR } from '@angular/fire/functions';
-import { DiscoverComponent } from './discover/discover.component';
-import { DiscoverParcelComponent } from './discover-parcel/discover-parcel.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { StructureUpgradeDialogComponent } from './structure-upgrade-dialog/structure-upgrade-dialog.component';
-import { WarRoomComponent } from './war-room/war-room.component';
-import { ReportsComponent } from './reports/reports.component';
-import { ReportDetailsComponent } from './report-details/report-details.component';
+import { providers } from 'ethers';
+
+// Services
+import { TileDataService } from './services/tile-data.service';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { CloudFunctionsService } from './services/cloud-functions.service';
+
+// Components
+import { GarrisonComponent } from './components/war-room-components/garrison/garrison.component';
+import { ExplorerComponent } from './components/dashboard-components/explorer/explorer.component';
+import { MapComponent } from './components/dashboard-components/map/map.component';
+import { BrowserComponent } from './components/dashboard-components/browser/browser.component';
+import { ParcelDetailsComponent } from './components/dashboard-components/parcel-details/parcel-details.component';
+import { DashboardComponent } from './components/dashboard-components/dashboard/dashboard.component';
+import { ParcelTileComponent } from './components/dashboard-components/parcel-tile/parcel-tile.component';
+import { HoverTileMenuComponent } from './components/dashboard-components/hover-tile-menu/hover-tile-menu.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { StructureComponent } from './components/dashboard-components/structure/structure.component';
+import { StructureDetailsComponent } from './components/dashboard-components/structure-details/structure-details.component';
+import { LoginComponent } from './components/login-components/login/login.component';
+import { DiscoverComponent } from './components/discover-components/discover/discover.component';
+import { DiscoverParcelComponent } from './components/discover-components/discover-parcel/discover-parcel.component';
+import { LandingPageComponent } from './components/landing-page-components/landing-page/landing-page.component';
+import { StructureUpgradeDialogComponent } from './components/dashboard-components/structure-upgrade-dialog/structure-upgrade-dialog.component';
+import { WarRoomComponent } from './components/war-room-components/war-room/war-room.component';
+import { ReportsComponent } from './components/report-components/reports/reports.component';
+import { ReportDetailsComponent } from './components/report-components/report-details/report-details.component';
+import { PlanningTableComponent } from './components/war-room-components/planning-table/planning-table.component';
 
 
 export function init_app(initalizeService: InitalizeService) {
@@ -82,7 +83,9 @@ const routes: Routes = [];
     StructureUpgradeDialogComponent,
     WarRoomComponent,
     ReportsComponent,
-    ReportDetailsComponent
+    ReportDetailsComponent,
+    GarrisonComponent,
+    PlanningTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/war-room-components/garrison/garrison.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-garrison',
+  templateUrl: './garrison.component.html',
+  styleUrls: ['./garrison.component.less']
+})
+export class GarrisonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
